3b57d1eca4cb0297883237c9b50f4b06e8eb05e1
Eric Torti <eric.torti@vivareal.com>
2015-02-03 19:07:31 -0200

    bringing back id on query for debugin purposes

--- scripts/fetch_dates.sh
+++ scripts/fetch_dates.sh
@@ -2,4 +2,4 @@ DB_HOST=$1
 DB_USER=$2
 DUMP_DIR=$3
 mkdir -p $DUMP_DIR
-psql -h $DB_HOST -d vivareal -U $DB_USER -W -t -A -F", " -c "select date_trunc('milliseconds', fch_actualizacion) from inmueble where estado='ACTIVO' and cuenta is not null and pcli = 'BR' order by date_trunc('milliseconds', fch_actualizacion) asc, id asc" > $DUMP_DIR/dump.csv
+psql -h $DB_HOST -d vivareal -U $DB_USER -W -t -A -F", " -c "select id, date_trunc('milliseconds', fch_actualizacion) from inmueble where estado='ACTIVO' and cuenta is not null and pcli = 'BR' order by date_trunc('milliseconds', fch_actualizacion) asc, id asc" > $DUMP_DIR/dump.csv
